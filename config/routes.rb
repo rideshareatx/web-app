ea0da9e16d758e5e587c72cafa28fb05a147cd66
CJ Jessett <cjessett@gmail.com>
2016-06-04 15:52:27 -0500

    Create user edit form field

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'route#index'
+
+  resources :route, :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
