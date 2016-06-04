a81228aac45c8226d4faccf22d4eeabe643b6da9
Daniel Zapata <daniel@peoplepattern.com>
2016-06-04 18:28:02 -0500

    make user edit form work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,31 @@ class UsersController < ApplicationController
 	end
 
 	def update
+    http = Net::HTTP.new(ENV['BACKEND_URL'], ENV['BACKEND_PORT'])
+    response = http.send_request('PUT', "/test-organization/newapp/user/#{current_user_id}?access_token=#{ENV['BACKEND_ACCESS_TOKEN']}", format_params.to_json)
+    @user = JSON.parse(response.body)['entities'].first
 	end
+
+  private
+
+  def format_params
+    {
+      adr: {
+        addr1: params[:home][:addr1],
+        addr2: params[:home][:addr2],
+        city: params[:home][:city],
+        state: params[:home][:state],
+        zip: params[:home][:zip],
+        country: params[:home][:country],
+      },
+      work_adr: {
+        addr1: params[:work][:addr1],
+        addr2: params[:work][:addr2],
+        city: params[:work][:city],
+        state: params[:work][:state],
+        zip: params[:work][:zip],
+        country: params[:work][:country],
+      }
+    }
+  end
 end
